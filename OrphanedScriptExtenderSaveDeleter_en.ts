33a30db1176ce73752a73044bd37997358980595
AnyOldName3 <krizdjali+github@gmail.com>
2018-05-08 14:59:02 +0100

    Add translation file

---
+++ OrphanedScriptExtenderSaveDeleter_en.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>OrphanedScriptExtenderSaveDeleter</name>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="45"/>
+        <source>Deletes script extender saves which don&apos;t have a corresponding base game save.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="42"/>
+        <source>Orphaned Script Extender Save Deleter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="55"/>
+        <source>Before starting deletion...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="55"/>
+        <source>Please double check that you want your orphaned script extender saves deleted. If you proceed, you won&apos;t be able to get them back, even if you find you&apos;ve copied the corresponding base game save somewhere else so still have a copy.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="71"/>
+        <source>No orphaned script extender saves found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="71"/>
+        <source>No orphaned script extender co-saves were found, so none were removed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="73"/>
+        <source>Orphaned script extender saves removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../OrphanedSESaves-plugin/modorganizer-orphaned_script_extender_save_deleter/OrphanedScriptExtenderSaveDeleter.py" line="73"/>
+        <source>{0} orphaned script extender co-save(s) removed successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
